940adf7f65e09f3adeefa23241a2e8205d60858e
ATRIKA SHOW <readeratrika08@gmail.com>
2024-01-23 20:49:46 +0530

    rendering data from data.json & using in ejs file

--- Express.js notes/InstaEjs/index.js
+++ Express.js notes/InstaEjs/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-let ejs = require('ejs');
 
 // ejs working
 app.set("view engine", "ejs");
@@ -12,12 +11,19 @@ app.get("/", (req, res)=> {
 });
 
 
+// Now we assume that, we are getting data from database (For now we are getting data from data.json file)
 app.get("/ig/:username", (req, res) =>{
-    let {username} = req.params;
-    res.render("page.ejs", {username});
+    let {username} = req.params;        // destructuring username
+
+    const data = require("./data.json");    // assuming this is our data coming from database 
+
+    // console.log(data.username);
+    //  By changing data.username to data[username], you are telling JavaScript to use the value of the username variable as the property name when accessing the data object
+    
+    res.render("page.ejs", {data : data[username]});    // rendering page.ejs by passing username
 })
 
 
 app.listen(8000, (req, res)=>{
-    console.log("app is listening")
+    console.log("app is listening");
 })
