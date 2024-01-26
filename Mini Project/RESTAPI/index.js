49da2a249be17c89d4006ea55bf7dd69b4501445
ATRIKA SHOW <readeratrika08@gmail.com>
2024-01-26 23:22:07 +0530

    Add new post Route

--- Mini Project/RESTAPI/index.js
+++ Mini Project/RESTAPI/index.js
@@ -43,6 +43,20 @@ app.get("/posts", (req, res)=>{
     res.render("index.ejs", {posts});
 })
 
+// 2nd Route -> Add new post
+// This will be a 2 step process
+
+app.get("/posts/new", (req, res) =>{    // 1. Adding new post in this route 
+    res.render("newpost.ejs");
+})
+
+app.post("/posts", (req, res) =>{      // 2. Handeling data in post request
+    // incoming data will be added in 'posts' array
+    let { name, content} = req.body;
+    posts.push({name, content});
+    res.send("post req working")
+})
+
 app.listen(port, ()=>{
     console.log(`App is listening on port ${port}`);
 })
