475f5d634e0b030fce6d5f97fefc26b2cd2f27b6
ATRIKA SHOW <readeratrika08@gmail.com>
2024-01-29 22:47:48 +0530

    Delete Route & some styling

--- Mini Project/MyPage/index.js
+++ Mini Project/MyPage/index.js
@@ -2,24 +2,30 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+var methodOverride = require('method-override')
 
-const posts = [
+let posts = [
     {
+        id : uuidv4(),
         img : "https://shorturl.at/gxEG9",
         caption : "Amazing time with nature",
         like : 100
     },
     {
+        id : uuidv4(),
         img : "https://shorturl.at/GQ146",
         caption : "beautiful time with nature",
         like : 200
     },
     {
+        id : uuidv4(),
         img : "https://shorturl.at/iuyG1",
         caption : "great time with nature",
         like : 600
     },
     {
+        id : uuidv4(),
         img : "https://shorturl.at/gxEG9",
         caption : "peaceful time with nature",
         like : 850
@@ -39,6 +45,9 @@ app.set("views", path.join(__dirname, "views"));
 // to server static file
 app.use(express.static(path.join(__dirname, "public")));
 
+// to override http request
+app.use(methodOverride('_method'));
+
 // 1st Route -> Main Page
 app.get("/", (req, res) =>{
     // here we will see main content like user name, no of post, etc
@@ -53,6 +62,15 @@ app.get("/posts", (req, res)=>{
 // 3rd Route -> to make a form for new post
 app.get("/posts/new", (req, res) =>{
     res.render("newPost.ejs");
+})
+
+// 4th Route -> Update feature (user can update caption)
+
+// 5th Route -> Delete Post
+app.delete("/posts/:id", (req, res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
 })
 
 // handle post request to create new post & it will redirect to all post after updating new post
